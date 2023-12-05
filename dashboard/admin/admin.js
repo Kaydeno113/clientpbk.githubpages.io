0bdd11501414b7826673f74214da72fe3b7361e8
Kaydeno113 <98924759+Kaydeno113@users.noreply.github.com>
2023-12-05 18:42:25 +0000

    update admin.js

--- dashboard/admin/admin.js
+++ dashboard/admin/admin.js
@@ -1,63 +1,46 @@
-// admin.js
-
-// Assuming you have already initialized Firebase
-
-// Fetch user data and populate the dropdown
-const userDropdown = document.getElementById('userDropdown');
-
-firebase.database().ref('users').once('value', function(snapshot) {
+// Initialize Firebase (replace this with your configuration)
+var firebaseConfig = {
+    apiKey: "AIzaSyC9fV9SVm7w16t7D8UannqxA49880cutJI",
+    authDomain: "clientspbk.firebaseapp.com",
+    projectId: "clientspbk",
+    storageBucket: "gs://clientspbk.appspot.com",
+    messagingSenderId: "792879644996",
+    appId: "1:792879644996:web:8a83f37d98697855029711"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  
+  // Get a reference to the users node
+  var usersRef = firebase.database().ref('users');
+  
+  // Get the user dropdown element
+  var userDropdown = document.getElementById('userDropdown');
+  
+  // Populate the user dropdown
+  usersRef.once('value', function(snapshot) {
     snapshot.forEach(function(userSnapshot) {
-        const userId = userSnapshot.key;
-        const userName = userSnapshot.val().name;
-
-        const option = document.createElement('option');
-        option.value = userId;
-        option.textContent = userName;
-        userDropdown.appendChild(option);
+      var userId = userSnapshot.key;
+      var userName = userSnapshot.child('name').val();
+  
+      var option = document.createElement('option');
+      option.value = userId;
+      option.text = userName;
+      userDropdown.add(option);
     });
-});
-
-// Handle form submission
-const uploadForm = document.getElementById('uploadForm');
-
-uploadForm.addEventListener('submit', function(event) {
+  });
+  
+  // Add event listener for the form submission
+  var uploadForm = document.getElementById('uploadForm');
+  uploadForm.addEventListener('submit', function(event) {
     event.preventDefault();
-
-    // Get selected user ID and other form data
-    const selectedUserId = userDropdown.value;
-    const photoFile = document.getElementById('photoInput').files[0];
-    const description = document.getElementById('descriptionInput').value;
-
-    // Upload photo to Firebase Storage
-    const storageRef = firebase.storage().ref().child('photos/' + selectedUserId + '/' + photoFile.name);
-    const uploadTask = storageRef.put(photoFile);
-
-    // Handle the upload progress and completion
-    uploadTask.on('state_changed',
-        function(snapshot) {
-            // Handle progress if needed
-            // const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            // console.log('Upload is ' + progress + '% done');
-        },
-        function(error) {
-            // Handle errors during upload
-            console.error('Upload error:', error);
-        },
-        function() {
-            // Upload successful, get download URL
-            uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-                // Save photo information to the user's node in the database
-                const photoData = {
-                    url: downloadURL,
-                    description: description
-                };
-
-                const photosRef = firebase.database().ref('users/' + selectedUserId + '/photos');
-                const newPhotoRef = photosRef.push();
-                newPhotoRef.set(photoData);
-
-                console.log('Photo uploaded successfully:', downloadURL);
-            });
-        }
-    );
-});
+  
+    // Get the selected user ID
+    var selectedUserId = userDropdown.value;
+  
+    // TODO: Implement the logic to upload the photo to the selected user's profile
+    // You can use the selectedUserId to determine which user's profile to update.
+    // For example, you can create a node like "users/{selectedUserId}/photos" to store photos.
+    // Use the File API to get the selected photo and upload it to Firebase Storage.
+    // Don't forget to update the photo's information (e.g., description).
+  });
+  
